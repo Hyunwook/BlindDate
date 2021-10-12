e749915abf8f3d8fdcea6811a2fcc505ff586329
강현욱 <84612861+Hyunwook@users.noreply.github.com>
2021-10-12 17:23:12 +0900

    Add files via upload

--- BlindDate/main.js
+++ BlindDate/main.js
@@ -3,9 +3,13 @@ $(window).on("load",function(){
     //home section slideshow
     let slideIndex = $(".slide.active").index();
     const slideLen = $(".slide").length;
-
+    $(function(){
+        $(".container").css("color","red")
+    });
+    
     function slideShow(){
         console.log(slideIndex)
+        $(".slide").removeClass("actitve").eq(slideIndex).addClass("active");
         if(slideIndex == slideLen -1){
             slideIndex = 0;
         }
